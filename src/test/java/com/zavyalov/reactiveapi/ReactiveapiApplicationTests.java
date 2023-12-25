c640c9cecd9c8487f186e37d8c56e61c8accf3ab
Daniil Zavyalov <dezavv1@gmail.com>
2023-12-25 15:50:51 +0600

    Initial commit

---
+++ src/test/java/com/zavyalov/reactiveapi/ReactiveapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zavyalov.reactiveapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
